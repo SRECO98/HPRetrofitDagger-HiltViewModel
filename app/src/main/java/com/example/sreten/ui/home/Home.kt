d45301f2e9f79f528af6aaccad47331912634ce3
Sreten <banovicbl22@gmail.com>
2022-10-26 13:48:41 +0200

    HomeViewModel

---
+++ app/src/main/java/com/example/sreten/ui/home/Home.kt
@@ -0,0 +1,2 @@
+package com.example.sreten.ui.home
+
---
+++ app/src/main/java/com/example/sreten/ui/home/HomeViewModel.kt
@@ -0,0 +1,31 @@
+package com.example.sreten.ui.home
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.sreten.data.api.model.Character
+import com.example.sreten.data.repository.CharacterRepo
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class HomeViewModel @Inject constructor(
+    private val characterRepo: CharacterRepo
+) : ViewModel() {
+
+    private val _state = MutableStateFlow(emptyList<Character>())
+    val state: StateFlow<List<Character>>
+    get() = _state //get() { return _state }
+/*ViewModel is a class that is responsible for preparing and managing the data for an Activity or a
+Fragment . It also handles the communication of the Activity / Fragment with the rest of the
+application (e.g. calling the business logic classes).*/
+    init {//However, suspending functions can only be invoked by another suspending function or within a coroutine.
+        viewModelScope.launch {    //// Coroutine that will be canceled when the ViewModel is cleared.
+            val characters = characterRepo.getCharacter()
+            _state.value = characters
+        }
+    }
+
+}
